77a81eec276f24a19cc6eeba5ff6c03160e713ba
Joel Calaón <joelcalaon22@gmail.com>
2026-01-06 01:41:46 -0300

    feat: tables per branch endpoint

---
+++ backend/src/data/tables.js
@@ -0,0 +1,15 @@
+export const tables = [
+    { id: "ros-1-t1", branchId: "ros-1", seats: 2},
+    { id: "ros-1-t2", branchId: "ros-1", seats: 2},
+    { id: "ros-1-t3", branchId: "ros-1", seats: 4},
+    { id: "ros-1-t4", branchId: "ros-1", seats: 8},
+    { id: "ros-1-t5", branchId: "ros-1", seats: 6},
+
+    { id: "ros-2-t1", branchId: "ros-2", seats: 2},
+    { id: "ros-2-t2", branchId: "ros-2", seats: 4},
+    { id: "ros-2-t3", branchId: "ros-2", seats: 6},
+
+    { id: "caba-1-t1", branchId: "caba-1", seats: 2},
+    { id: "caba-1-t1", branchId: "caba-1", seats: 8},
+    { id: "caba-1-t1", branchId: "caba-1", seats: 4},
+];
--- backend/src/routes/branches.routes.js
+++ backend/src/routes/branches.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { branches } from "../data/branches.js";
+import { tables } from "../data/tables.js";
 
 const router = Router();
 
@@ -18,6 +19,18 @@ router.get("/", (req, res) => {
     res.json(result);
 });
 
+router.get("/:id/tables", (req, res) => {
+    const branchid = req.params.id;
+
+    const branch = branches.find(b => b.id === branchid);
+    if(!branch) {
+        return res.status(404).json({ message: "branch not found"});
+    }
+
+    const branchTables = tables.filter(t => t.branchId === branchid);
+    res.json(branchTables);
+});
+
 router.get("/:id", (req, res) => {
     const branch = branches.find(b => b.id === req.params.id);
     
@@ -27,5 +40,6 @@ router.get("/:id", (req, res) => {
 
     res.json(branch);
 });
+
 
 export default router;
